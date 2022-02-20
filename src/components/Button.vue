be79a47ac8510d16a17563f3035bd1502ce5e0c6
Артур Филимонцев <homerboy19520@mail.ru>
2022-02-20 17:11:34 +0300

    добавил кнопку

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="button" :class="[`m-${modifier}`]">
+    {{ content }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+
+    modifier: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.button {
+  box-sizing: border-box;
+  font-weight: 400;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+}
+</style>

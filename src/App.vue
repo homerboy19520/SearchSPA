d74529c74f0d06b698f8e2a42bae3ac04084eb22
Артур Филимонцев <homerboy19520@mail.ru>
2022-02-20 13:43:34 +0300

    Add project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <SearchSpa></SearchSpa>
+</template>
+
+<script>
+import SearchSpa from './pages/SearchSpa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SearchSpa
+  }
+}
+</script>
+
+<style>
+@import "assets/styles/main.css";
+</style>
---
+++ src/pages/SearchSpa.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="search-spa"></div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchSpa',
+}
+</script>
+
+<style>
+</style>
